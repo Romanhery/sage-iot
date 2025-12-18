78f9315ee57ab0f0eeb4c531495e012fbc8dab3b
Inatlus Namor <inatlusnamor@gmail.com>
2025-12-17 19:24:03 -0800

    Update login and sign-up links to new paths

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <h1><a href="/login">Login</a></h1>
-        <h1><a href="/sign-up">Sign Up</a></h1>
+        <h1><a href="/auth/login">Login</a></h1>
+        <h1><a href="/auth/sign-up">Sign Up</a></h1>
       </div>
     </main>
   );
